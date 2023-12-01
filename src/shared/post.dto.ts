5304e16017149489f25900d0f09320d3629faf94
linh14dev <linh14dev@gmail.com>
2023-12-02 02:38:55 +0700

    Update Post

--- src/shared/post.dto.ts
+++ src/shared/post.dto.ts
@@ -2,6 +2,7 @@ import { IsLatitude, IsLongitude, IsNotEmpty, IsOptional } from "class-validator
 import { type Prisma } from "@prisma/client";
 export class CreatePostDTO {
     content: string
+    star: number
     user: Prisma.UserCreateNestedOneWithoutPostInput
     service: Prisma.ServiceCreateNestedOneWithoutPostInput
     long?: number
